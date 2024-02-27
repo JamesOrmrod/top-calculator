ac3fd009a3cc31f261cdebb2148e2e26d4aee163
James Ormrod <88646794+JamesOrmrod@users.noreply.github.com>
2024-02-27 12:07:36 +1100

    Add keyboard input

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -7,25 +7,30 @@ const screen = document.querySelector("#screen");
 const numButtons = document.querySelectorAll(".num-button");
 
 numButtons.forEach((button) => {
-    button.addEventListener('click', (e) => pressNumButton(button));
+    button.addEventListener('click', () => pressNumButton(button.textContent));
 });
 
+const clrButton = document.querySelector("#clr");
+clrButton.addEventListener('click', () => clearScreen("button"));
 
-const clrButton = document.querySelector(".fn-button#clr");
-clrButton.addEventListener('click', () => clearScreen());
-
-const equalsButton = document.querySelector(".fn-button#equals");
-equalsButton.addEventListener('click', () => calculate());
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener('click', calculate);
 
 const operButtons = document.querySelectorAll(".operator-button");
 
 operButtons.forEach((button) => {
-    button.addEventListener('click', (e) => pressOperButton(button));
+    button.addEventListener('click', (e) => pressOperButton(button.textContent));
 })
 
-const decimButton = document.querySelector(".button#decimal");
+const decimButton = document.querySelector("#decimal");
+decimButton.addEventListener('click', pressDecimal);
 
-decimButton.addEventListener('click', () => pressDecimal());
+const deleteButton = document.querySelector("#delete");
+deleteButton.addEventListener('click', pressDelete);
+
+// add a listener for keypresses to call functions
+document.addEventListener('keydown', pressKey);
+
 
 const operators = {
     '+': (a, b) => a + b,
@@ -34,22 +39,46 @@ const operators = {
     '÷': (a, b) => a / b,
 }
 
+// handle the keyboard press and call the correct function
+function pressKey(e) {
+    key = e.key;
+    if (/[0-9]/.test(key)) {
+        pressNumButton(key);
+    } else if (key === ".") {
+        pressDecimal();
+    } else if (/[\/\*+-]/.test(key)) {
+        switch (key) {
+            case "/": pressOperButton("÷");
+            case "*": pressOperButton("x");
+            case "+": pressOperButton("+");
+            case "-": pressOperButton("-");
+        };
+    } else if (key === "Enter" || key === "=") {
+        calculate();
+    } else if (key === "Backspace") {
+        pressDelete();
+    } else if (key === "Delete") {
+        clearScreen("delete key");
+    }
+}
+
 // store the input in an array for use when calculating and update the screen
 function pressNumButton(input) {
     if (inputArray.length % 2 === 0) {
-        inputArray.push(input.textContent);
+        inputArray.push(input);
     } else {
-        inputArray[inputArray.length - 1] += input.textContent;
+        inputArray[inputArray.length - 1] += input;
     }
     updateDisplay();
 }
 
 function pressOperButton(input) {
     if (inputArray.length === 1) {
-        inputArray.push(input.textContent); 
+        inputArray.push(input); 
+        updateDisplay();
     } else if (inputArray.length === 3) {
         calculate();
-        inputArray.push(input.textContent);
+        inputArray.push(input);
     } 
 }
 
@@ -66,6 +95,22 @@ function pressDecimal() {
     updateDisplay();
 }
 
+function pressDelete() {
+    currentIndex = inputArray.length - 1;
+    current = inputArray[currentIndex];
+
+    if (currentIndex >= 0) {
+        if (current.length < 2) {
+            inputArray.pop();
+        } else if (current.length > 1) {
+            inputArray[currentIndex] = current.slice(0, -1);
+        }
+        updateDisplay();
+    }
+}
+
+
+
 // update the screen display
 function updateDisplay() {
     if (inputArray.length === 0) {
@@ -78,7 +123,8 @@ function updateDisplay() {
 }
 
 
-function clearScreen() {
+function clearScreen(src) {
+    console.log('clearing screen from source: ' + src);
     inputArray = [];
     updateDisplay();
 }
@@ -99,8 +145,13 @@ function calculate() {
         let result = 0;
         
         result = operators[oper](num1, num2);
-        inputArray = [];
-        inputArray[0] = result;
+
+        newArr = [result];
+        inputArray = newArr;
+
+        console.log(newArr);
+        console.log(inputArray);
+
         updateDisplay();
 
         console.log(`num1 = ${num1}, num2 = ${num2}, oper = ${oper}`);
